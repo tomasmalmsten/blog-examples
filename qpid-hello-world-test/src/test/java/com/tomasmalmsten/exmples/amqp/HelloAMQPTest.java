e2c2c62ba3da83ca730c37c6c7ab91e066a9f9ea
Tomas Malmsten <tomas.malmsten@mobill.se>
2013-11-01 11:22:39 +0100

    Remove the qpid-hello-world-test project from regular builds. It requires a running RabbitMQ server for the test not to fail.

--- qpid-hello-world-test/src/test/java/com/tomasmalmsten/exmples/amqp/HelloAMQPTest.java
+++ qpid-hello-world-test/src/test/java/com/tomasmalmsten/exmples/amqp/HelloAMQPTest.java
@@ -1,66 +1,71 @@
 package com.tomasmalmsten.exmples.amqp;
 
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This test will atempt to open a connection to a running RabbitMQ server. If
+ * the system does not have a running RabbitMQ server the test will faile with
+ * a ConnectionExcepion in the setupConnectionAndChannelAndExchange method.
+ */
 public class HelloAMQPTest {
 
-    private static final String EXCHANGE_NAME = "hello";
-    private static final String ROUTING_KEY = "routingkey";
-    private static final String MESSAGE = "Hello World";
-    private static final boolean AUTO_ACK = false;
-    private static final String TYPE = "direct";
-    private static final boolean DURABILITY = true;
-    private Channel channel;
-    private Connection connection;
-    private String queueName;
-    private String message;
+  private static final String EXCHANGE_NAME = "hello";
+  private static final String ROUTING_KEY = "routingkey";
+  private static final String MESSAGE = "Hello World";
+  private static final boolean AUTO_ACK = false;
+  private static final String TYPE = "direct";
+  private static final boolean DURABILITY = true;
+  private Channel channel;
+  private Connection connection;
+  private String queueName;
+  private String message;
 
+  @Before
+  public void setUpConnectionAndChannelAndExchange() throws IOException {
+    ConnectionFactory factory = new ConnectionFactory();
+    connection = factory.newConnection();
+    channel = connection.createChannel();
+    channel.exchangeDeclare(EXCHANGE_NAME, TYPE, DURABILITY);
+    queueName = channel.queueDeclare().getQueue();
+    channel.queueBind(queueName, EXCHANGE_NAME, ROUTING_KEY);
+  }
 
-    @Before
-    public void setUpConnectionAndChannelAndExchange() throws IOException {
-        ConnectionFactory factory = new ConnectionFactory();
-        connection = factory.newConnection();
-        channel = connection.createChannel();
-        channel.exchangeDeclare(EXCHANGE_NAME, TYPE, DURABILITY);
-        queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, EXCHANGE_NAME, ROUTING_KEY);
-    }
+  @Test
+  public void receiveSentMessage() throws InterruptedException, IOException {
+    givenMessageIsSentThroughConfiguredChannel();
+    whenTheMessageIsRetrievedFromTheChannel();
+    thenTheReceivedMessageShouldMatchTheSentMessage();
+  }
 
-    @Test
-    public void receiveSentMessage() throws InterruptedException, IOException {
-        givenMessageIsSentTrhoughConfiguredChannel();
-        whenTheMessageIsRetrivedFromTheChannel();
-        thenTheReceivedMessageShouldMatchTheSentMessage();
-    }
+  private void givenMessageIsSentThroughConfiguredChannel() throws IOException {
+    channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, null, MESSAGE.getBytes());
+  }
 
-    private void givenMessageIsSentTrhoughConfiguredChannel() throws IOException {
-        channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, null, MESSAGE.getBytes());
-    }
+  private void whenTheMessageIsRetrievedFromTheChannel() throws IOException, InterruptedException {
+    QueueingConsumer consumer = new QueueingConsumer(channel);
+    channel.basicConsume(queueName, AUTO_ACK, consumer);
+    QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+    message = new String(delivery.getBody());
+    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+  }
 
-    private void whenTheMessageIsRetrivedFromTheChannel() throws IOException, InterruptedException {
-        QueueingConsumer consumer = new QueueingConsumer(channel);
-        channel.basicConsume(queueName, AUTO_ACK, consumer);
-        QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-        message = new String(delivery.getBody());
-        channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-    }
+  private void thenTheReceivedMessageShouldMatchTheSentMessage() {
+    assertEquals(MESSAGE, message);
+  }
 
-    private void thenTheReceivedMessageShouldMatchTheSentMessage() {
-        assertEquals(MESSAGE, message);
-    }
-
-    @After
-    public void closeChannelAndConnection() throws IOException {
-        channel.close();
-        connection.close();
-    }
+  @After
+  public void closeChannelAndConnection() throws IOException {
+    channel.close();
+    connection.close();
+  }
 }
